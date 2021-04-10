0f6dba1c74bc5f68536adba96c0d581d34818130
gelickonova <lera.gelickonova@gmail.com>
2021-04-10 09:45:07 +0300

    module 25

--- app.js
+++ app.js
@@ -10,10 +10,34 @@ app.use(express.static('dist'))
 
 mongoose.connect('mongodb://localhost/sf-drive-users', function (err) {
     if (err) throw err;
-    console.log('Successfully connected');
+    console.log('Successfully connected!!');
 
 });
 
+const userSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    birthDate: Date,
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: Number,
+    passportID: Number,
+    dateOfIssue: Date,
+    issuingAuthority: String,
+    departmentCode: Number,
+    numberOfLicense: Number,
+    dateOfLicense: Date,
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
 
 app.get('/aboutus', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'))
@@ -51,8 +75,28 @@ app.post("/registration/step1", function (req, res) {
     if (!user.email.match(emailRegex)) errors.email = true;
     if (errors.password || errors.email) {
         res.status(500).send(errors)
-    } else
+    } else {
+
+        let someUser = new User(
+            {
+                fullName: user.fullName,
+                birthDate: user.birthDate,
+                email: user.email,
+                phoneNumber: user.phoneNumber,
+                passportID: user.passportID,
+                dateOfIssue: user.dateOfIssue,
+                issuingAuthority: user.issuingAuthority,
+                departmentCode: user.departmentCode,
+                numberOfLicense: user.numberOfLicense,
+                dateOfLicense: user.dateOfLicense,
+                password: user.password
+            })
+        someUser.save(err => {
+            if (err) throw err;
+            console.log('user successfully saved!')
+        })
         res.sendStatus(200)
+    }
 })
 
 app.post("/registration/step2", function (req, res) {
